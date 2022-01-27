6d0a0cb15dc42c07d6699ccbd163330c260ff278
Elif <elifsabuncular8@gmail.com>
2022-01-26 23:19:45 -0500

    hi

--- CodingTrainExtTut/basic/content.js
+++ CodingTrainExtTut/basic/content.js
@@ -5,3 +5,9 @@ let paragraphs = document.getElementsByTagName('p')
 for (elt of paragraphs) {
     elt.style['background-color'] = '#00FF00';
 }
+
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage(message, sender, sendResponse) {
+    console.log(message.txt);
+}
